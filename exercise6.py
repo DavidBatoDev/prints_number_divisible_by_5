8871735e0f23ff13651af96f95c47771d85d0363
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-01-22 15:06:29 +0800

    Finished Code!

--- exercise6.py
+++ exercise6.py
@@ -1,4 +1,8 @@
 given_list = [10, 20, 33, 46, 55]
+print(f"Given List is {given_list}")
 print("Divisible by 5")
 # loop through the given list
+for number in given_list:
+    if number % 5 == 0:
+        print(number)
 # print only the item that is divisible by 5
